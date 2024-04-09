a57111ff1cfc0e15b0789cd17ba69d82948c2991
Sakib-2004043 <sakib1514817122@gmail.com>
2024-04-09 06:13:02 +0600

    6.12

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = 5000;
 
 // dotENV **** It Only Works On BACKEND ****
 require('dotenv').config();
